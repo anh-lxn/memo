2d62caa6d12fb213d1661a1a2df6bb3fd18ab6bd
flsc920f <flsc920f@tu-dresden.de>
2024-10-28 15:11:56 +0100

    updating reading_sensordata_pi_adafruit.py

--- src/main/python/Erfassung_Trainingsdaten/01_reading_sensordata_pi_adafruit.py
+++ src/main/python/Erfassung_Trainingsdaten/01_reading_sensordata_pi_adafruit.py
@@ -1,75 +1,45 @@
-"""
-Dateiname: reading_sensordata_pi.py
-Autor: Florian Schmidt
-Datum: 28.10.2024
-
-Beschreibung:
-Diese Datei dient dem Aufnehmen von Sensordaten (8 Sensoren) mittels Raspberry Pi und Adafruit 1115 AD-Wandlern.
-Die ausgelesenen Daten werden in einer .csv Datei abgelegt.
-"""
-
 # Imports
 import os
 import csv
 from datetime import datetime
 import threading
-
-# User Inputs
-id = 33 # id for the position of the load point
-x = 290 # x-position of the load point
-y = 174 # y-position of the load point
-F = 50 # force (load) in Newton
-dt = 0.25 # sleep time between saved data
-
-# Liste zur Speicherung der Daten (8 Sensoren)
-data = []
-
 import time
 import board
 import busio
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
-# I2C setup (ads1115 uses following i2c adresses: 0x48, 0x49, 0x4A, 0x4B)
+# I2C setup
 i2cbus = busio.I2C(board.SCL, board.SDA)
 ads0 = ADS.ADS1115(i2cbus, address=0x48)
 ads1 = ADS.ADS1115(i2cbus, address=0x49)
 
 # Set up analog channels
-ch1 = AnalogIn(ads0, ADS.P0) # entspricht Sensor x
-ch2 = AnalogIn(ads0, ADS.P1) # entspricht Sensor x
-ch3 = AnalogIn(ads0, ADS.P2) # entspricht Sensor x
-ch4 = AnalogIn(ads0, ADS.P3) # entspricht Sensor x
-ch5 = AnalogIn(ads1, ADS.P0) # entspricht Sensor x
-ch6 = AnalogIn(ads1, ADS.P1) # entspricht Sensor x
-ch7 = AnalogIn(ads1, ADS.P2) # entspricht Sensor x
-ch8 = AnalogIn(ads1, ADS.P3) # entspricht Sensor x
+ch1, ch2, ch3, ch4 = AnalogIn(ads0, ADS.P0), AnalogIn(ads0, ADS.P1), AnalogIn(ads0, ADS.P2), AnalogIn(ads0, ADS.P3)
+ch5, ch6, ch7, ch8 = AnalogIn(ads1, ADS.P0), AnalogIn(ads1, ADS.P1), AnalogIn(ads1, ADS.P2), AnalogIn(ads1, ADS.P3)
 
+# Variable zum Stoppen des Threads
+stop_event = threading.Event()
 
 # Funktion zum Speichern der Daten in eine CSV-Datei
 def save_to_csv(data, output_dir, ids, x, y, F):
     output_csv_path = os.path.join(output_dir, f'0{ids}_strain_values_{x}_{y}_{F}.csv')
     with open(output_csv_path, mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['Timestamp', 'dt','Voltage1', 'Voltage2', 'Voltage3', 'Voltage4', 'Voltage5', 'Voltage6', 'Voltage7', 'Voltage8'])
+        writer.writerow(['Timestamp', 'dt', 'Voltage1', 'Voltage2', 'Voltage3', 'Voltage4', 'Voltage5', 'Voltage6', 'Voltage7', 'Voltage8'])
         writer.writerows(data)
     print(f'Daten wurden in {output_csv_path} gespeichert.')
 
 # Funktion zum kontinuierlichen Auslesen der Sensordaten
 def read_sensors():
-    global dt, ch1,ch2,ch3,ch4,ch5,ch6,ch7,ch8
-    while not stop_thread:
-        curr_voltages = [ch1.voltage,ch2.voltage,ch3.voltage,ch4.voltage,ch5.voltage,ch6.voltage,ch7.voltage,ch8.voltage]
+    while not stop_event.is_set():
+        curr_voltages = [ch1.voltage, ch2.voltage, ch3.voltage, ch4.voltage, ch5.voltage, ch6.voltage, ch7.voltage, ch8.voltage]
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         data.append([timestamp, dt] + curr_voltages)
         time.sleep(dt)
 
-# Variable zum Stoppen des Threads
-stop_thread = False
-
 # Funktion zur Eingabe der Parameter über die Kommandozeile
 def get_user_inputs():
-    global ids, x, y, F
     ids = int(input("Geben Sie die ID des Lastpunkts ein: "))
     x = int(input("Geben Sie die X-Position ein: "))
     y = int(input("Geben Sie die Y-Position ein: "))
@@ -79,10 +49,8 @@ def get_user_inputs():
 # Hauptprogramm Ausführung
 if __name__ == "__main__":
     # Benutzerparameter abfragen
-    get_user_inputs()
-
-    # Benutzerdefinierte Variablen
-    dt = 0.25  # sleep time between saved data
+    ids, x, y, F = get_user_inputs()
+    dt = 0.25
     data = []
 
     # Erstelle den Ordner "messungen_aktuelles-datum-zeit"
@@ -101,5 +69,4 @@ if __name__ == "__main__":
     thread.join()  # Warte, bis der Thread beendet ist
 
     # Speichere die Daten, wenn das Programm beendet wird
-    save_to_csv(data, output_dir, id, x, y, F)
-
+    save_to_csv(data, output_dir, ids, x, y, F)
